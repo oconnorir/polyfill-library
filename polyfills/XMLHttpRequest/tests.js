e8094cc59a77edd5f52a9ca2e5282d87259a6c88
Jake Champion <jakechampion.jake2@googlemail.com>
2019-01-04 17:04:50 +0000

    remove skipped tests

--- polyfills/XMLHttpRequest/tests.js
+++ polyfills/XMLHttpRequest/tests.js
@@ -7,21 +7,6 @@ function nameOf(fn) {
 	return Function.prototype.toString.call(fn).match(/function\s*([^\s]*)\s*\(/)[1];
 }
 
-// REMOVED: Safari considers XMLHttpRequest an object
-it.skip('has correct instance', function () {
-	proclaim.isInstanceOf(XMLHttpRequest, Function);
-});
-
-// REMOVED: Firefox considers XMLHttpRequest a function, but does not allow you to use Function.prototype.toString (yet accepts XMLHttpRequest.toString!)
-it.skip('has correct name', function () {
-	proclaim.equal(nameOf(XMLHttpRequest), 'XMLHttpRequest');
-});
-
-// REMOVED: See above
-it.skip('has correct argument length', function () {
-	proclaim.equal(XMLHttpRequest.length, 0);
-});
-
 it('has correct states', function () {
 	proclaim.equal(XMLHttpRequest.UNSENT, 0);
 	proclaim.equal(XMLHttpRequest.OPENED, 1);
@@ -43,14 +28,6 @@ it('can setup request', function () {
 
 	proclaim.isInstanceOf(xhr.send, Function);
 	proclaim.equal(nameOf(xhr.send), 'send');
-});
-
-// REMOVED: Inconsistent between browsers, does not affect functionality
-it.skip('has methods with correct argument length', function() {
-	proclaim.equal(xhr.open.length, 2);
-	proclaim.equal(xhr.addEventListener.length, 0);
-	proclaim.equal(xhr.send.length, 0);
-
 });
 
 it('can make request', function () {
