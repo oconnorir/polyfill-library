8507c236485a14cfb20f1d9f1ff1431d36d57add
Jake Champion <jakechampion.jake2@googlemail.com>
2019-01-29 16:17:06 +0000

    Improve polyfill and tests for Number.parseInt

--- polyfills/Number/parseInt/polyfill.js
+++ polyfills/Number/parseInt/polyfill.js
@@ -1,4 +1,11 @@
 /* global CreateMethodProperty */
-// 20.1.2.13. Number.parseInt ( string, radix )
-// The value of the Number.parseInt data property is the same built-in function object that is the value of the  parseInt property of the global object defined in 18.2.5.
-CreateMethodProperty(Number, 'parseInt', parseInt);
+(function (nativeParseInt, global) {
+	// Polyfill.io - IE 8's parseInt is incorrect
+    CreateMethodProperty(global, 'parseInt', function parseInt(str, radix) {
+        var string = String(str).trim();
+        return nativeParseInt(string, (radix >>> 0) || (/^[-+]?0[xX]/.test(string) ? 16 : 10));
+    });
+	// 20.1.2.13. Number.parseInt ( string, radix )
+	// The value of the Number.parseInt data property is the same built-in function object that is the value of the  parseInt property of the global object defined in 18.2.5.
+	CreateMethodProperty(Number, 'parseInt', global.parseInt);
+}(parseInt, this));
